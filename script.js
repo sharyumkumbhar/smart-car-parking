41f3dc54dd33413ec24fa7b194fa135da056c61a
aryan <sharyumkumbhar@gmail.com>
2026-07-26 22:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-alert("JavaScript Connected Successfully!");
-console.log("Smart Car Parking Loaded");
+const SUPABASE_URL = "https://yokdshtjbjlhqmmrnpxh.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_2ZDoGT6xIclKRzqbE78Vnw_Uwp7BRdK";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+alert("Supabase Connected Successfully!");
